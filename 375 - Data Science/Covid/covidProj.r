84bdeebb724ed70c9d279ba751c1b4e4b6d05aad
naokishami <naokishami@gmail.com>
2021-04-06 00:47:14 -0700

    Refactored R code

--- 375 - Data Science/Covid/covidProj.r
+++ 375 - Data Science/Covid/covidProj.r
@@ -7,30 +7,27 @@ demo <- read_csv("/Users/naoki_atkins/Desktop/demographics.csv")
 
 # Tidying the data
 covid.tidy <- covid %>% 
-  pivot_longer(cols = 5:442, names_to = "Date", values_to = "cases")
+  pivot_longer(cols = 5:ncol(covid), names_to = "Date", values_to = "cases")
 
-bed.tidy <- bed %>% 
-  group_by(Country) %>%
-  filter(Year == max(Year))
-
+# Tidying the data
 demo.tidy <- demo %>% 
   group_by(`Country Code`) %>% 
   select(-`Series Name`) %>% 
-  pivot_wider(names_from = `Series Code`, values_from = `YR2015`)
+  pivot_wider(names_from = `Series Code`, values_from = `YR2015`) %>%
+  ungroup()
 
-demo.tidy <- demo.tidy %>%
-  mutate(SP.POP.80UP.TOTL = SP.POP.80UP.FE + SP.POP.80UP.MA)
+# Extract the most recent year for every country
+bed.tidy <- bed %>% 
+  group_by(Country) %>%
+  filter(Year == max(Year)) %>%
+  ungroup()
 
+# Add male and female populations together
 demo.tidy <- demo.tidy %>%
-  mutate(SP.POP.1564.TOTL = SP.POP.1564.FE.IN + SP.POP.1564.MA.IN)
-  
-demo.tidy <- demo.tidy %>%
-  mutate(SP.POP.0014.TOTL = SP.POP.0014.FE.IN + SP.POP.0014.MA.IN)
-  
-demo.tidy <- demo.tidy %>%
-  mutate(SP.DYN.AMRT.TOTL = SP.DYN.AMRT.FE + SP.DYN.AMRT.MA)
-
-demo.tidy <- demo.tidy %>%
+  mutate(SP.POP.80UP.TOTL = SP.POP.80UP.FE + SP.POP.80UP.MA) %>%
+  mutate(SP.POP.1564.TOTL = SP.POP.1564.FE.IN + SP.POP.1564.MA.IN) %>%
+  mutate(SP.POP.0014.TOTL = SP.POP.0014.FE.IN + SP.POP.0014.MA.IN) %>%
+  mutate(SP.DYN.AMRT.TOTL = SP.DYN.AMRT.FE + SP.DYN.AMRT.MA) %>%
   mutate(SP.POP.65UP.TOTL = SP.POP.65UP.FE.IN + SP.POP.65UP.MA.IN)
 
 # Putting countries into its own variable. 
@@ -39,6 +36,9 @@ bed.countries <- bed.tidy %>% distinct(Country) %>% ungroup()
 covid.countries <- covid.tidy %>% distinct(`Country/Region`)
 demo.countries <- demo.tidy %>% distinct(`Country Name`) %>% ungroup() %>% select(`Country Name`)
 
-bed.tidy <- bed.tidy %>% mutate(Country = replace(Country, Country == "Republic of Korea", "South Korea"))
-
-
+bed.tidy <- bed.tidy %>% 
+  mutate(Country = replace(Country, Country == "Republic of Korea", "South Korea")) %>%
+  mutate(Country = replace(Country, Country == "Democratic People's Republic of Korea", "North Korea")) %>%
+  mutate(Country = replace(Country, Country == "Iran (Islamic Republic of)", "Iran")) %>%
+  mutate(Country = replace(Country, Country == "United Kingdom of Great Britain and Northern Ireland", "United Kingdom"))
+  
