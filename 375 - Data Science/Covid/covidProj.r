ef045e77995cf1925be250736bd607f4a1c3c0d7
naokishami <naokishami@gmail.com>
2021-04-06 21:59:37 -0700

    Wrangling portion complete

--- 375 - Data Science/Covid/covidProj.r
+++ 375 - Data Science/Covid/covidProj.r
@@ -1,13 +1,19 @@
 library(tidyverse)
+library(lubridate) # used to order by date
 
 # Read the datasets
 covid <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
+covidDeath <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
 bed <- read_csv("/Users/naoki_atkins/Desktop/data.csv")
 demo <- read_csv("/Users/naoki_atkins/Desktop/demographics.csv")
 
 # Tidying the data
 covid.tidy <- covid %>% 
-  pivot_longer(cols = 5:ncol(covid), names_to = "Date", values_to = "cases")
+  pivot_longer(cols = 5:ncol(covid), names_to = "Date", values_to = "confirmed")
+
+# Tidying the data
+covidDeath.tidy <- covidDeath %>% 
+  pivot_longer(cols = 5:ncol(covidDeath), names_to = "Date", values_to = "deaths")
 
 # Tidying the data
 demo.tidy <- demo %>% 
@@ -16,13 +22,13 @@ demo.tidy <- demo %>%
   pivot_wider(names_from = `Series Code`, values_from = `YR2015`) %>%
   ungroup()
 
-# Extract the most recent year for every country
+# Extract the most recent year for every country - all other years are not necessary to keep
 bed.tidy <- bed %>% 
   group_by(Country) %>%
   filter(Year == max(Year)) %>%
   ungroup()
 
-# Add male and female populations together
+# Add male and female populations together - not examining sex-related differences
 demo.tidy <- demo.tidy %>%
   mutate(SP.POP.80UP.TOTL = SP.POP.80UP.FE + SP.POP.80UP.MA) %>%
   mutate(SP.POP.1564.TOTL = SP.POP.1564.FE.IN + SP.POP.1564.MA.IN) %>%
@@ -37,11 +43,16 @@ bed.tidy <- bed.tidy %>%
 # Rename header
 covid.tidy <- covid.tidy %>% 
   rename(ProvinceState = `Province/State`) %>%
-  rename(CountryRegion = `Country/Region`)
+  rename(Country = `Country/Region`)
+
+# Rename header
+covidDeath.tidy <- covidDeath.tidy %>% 
+  rename(ProvinceState = `Province/State`) %>%
+  rename(Country = `Country/Region`)
 
 # Rename header
 demo.tidy <- demo.tidy %>% 
-  rename(CountryName = `Country Name`) %>%
+  rename(Country = `Country Name`) %>%
   rename(CountryCode = `Country Code`)
 
 # Rename countries in a standard format so we can join tables later
@@ -53,15 +64,40 @@ bed.tidy <- bed.tidy %>%
 
 # Renaming countries
 covid.tidy <- covid.tidy %>% 
-  mutate(CountryRegion = replace(CountryRegion, CountryRegion == "Korea, South", "South Korea"))
+  mutate(Country = replace(Country, Country == "Korea, South", "South Korea"))
+
+# Renaming countries
+covidDeath.tidy <- covidDeath.tidy %>% 
+  mutate(Country = replace(Country, Country == "Korea, South", "South Korea"))
 
 # Renaming countries
 demo.tidy <- demo.tidy %>% 
-  mutate(CountryName = replace(CountryName, CountryName == "Korea, Rep.", "South Korea")) %>%
-  mutate(CountryName = replace(CountryName, CountryName == "Korea, Dem. People’s Rep.", "North Korea")) %>%
-  mutate(CountryName = replace(CountryName, CountryName == "Iran, Islamic Rep.", "Iran"))
-  
-  
+  mutate(Country = replace(Country, Country == "Korea, Rep.", "South Korea")) %>%
+  mutate(Country = replace(Country, Country == "Korea, Dem. People’s Rep.", "North Korea")) %>%
+  mutate(Country = replace(Country, Country == "Iran, Islamic Rep.", "Iran"))
+
+# Summing up all the provinces/state into country
+covid.tidy <- covid.tidy %>% 
+  group_by(Country, Date) %>% 
+  summarise(confirmed = sum(confirmed)) %>%
+  arrange(mdy(Date)) %>%
+  ungroup()
+
+# Summing up all the provinces/state into country
+covidDeath.tidy <- covidDeath.tidy %>% 
+  group_by(Country, Date) %>% 
+  summarise(deaths = sum(deaths)) %>%
+  arrange(mdy(Date)) %>%
+  ungroup()
+
+covid.table <- covid.tidy %>% 
+  full_join(covidDeath.tidy) %>% 
+  full_join(bed.tidy) %>% 
+  full_join(demo.tidy) 
+
+covid.table <- covid.table %>% 
+  select(-Year, -CountryCode)
+
 
 
 
